ac21ad4a80a43d94372b03a800d1154cd01a819e
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-05-19 14:51:28 -0300

    feat: feature para curtir livro

--- organo/src/app/components/livro/livro.component.ts
+++ organo/src/app/components/livro/livro.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './livro.component.css'
 })
 export class LivroComponent {
+
+  alternarFavorito(){
+    this.livro.favorito = !this.livro.favorito;
+  }
+
   livro = {
     titulo: "As ondas",
     autoria: "Virginia Wolf",
-    capa: "https://books.google.com.br/books/publisher/content?id=K88vEAAAQBAJ&hl=pt-BR&pg=PP1&img=1&zoom=3&bul=1&sig=ACfU3U17ilHei2sAJISd9dgpqzbnbgyUBw&w=1280"
+    capa: "https://books.google.com.br/books/publisher/content?id=K88vEAAAQBAJ&hl=pt-BR&pg=PP1&img=1&zoom=3&bul=1&sig=ACfU3U17ilHei2sAJISd9dgpqzbnbgyUBw&w=1280",
+    favorito: false
   }
 }
