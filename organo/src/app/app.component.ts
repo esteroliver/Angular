87e999adbeeeca047b9100893092d343c5b15ccb
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-05-19 13:18:48 -0300

    feat: componente de rodapé

--- organo/src/app/app.component.ts
+++ organo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
