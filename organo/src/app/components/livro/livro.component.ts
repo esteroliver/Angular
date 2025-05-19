ee7417954cedad423bb01d0c6a3e7cb3c42ded76
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-05-19 15:28:33 -0300

    feat: realizando alterações no componente Livro

--- organo/src/app/components/livro/livro.component.ts
+++ organo/src/app/components/livro/livro.component.ts
@@ -18,6 +18,9 @@ export class LivroComponent {
     autoria: "Virginia Wolf",
     capa: "https://books.google.com.br/books/publisher/content?id=K88vEAAAQBAJ&hl=pt-BR&pg=PP1&img=1&zoom=3&bul=1&sig=ACfU3U17ilHei2sAJISd9dgpqzbnbgyUBw&w=1280",
     favorito: false,
-    genero: "Drama"
+    genero: {
+      id: "romance",
+      value: "romance"
+    }
   }
 }
