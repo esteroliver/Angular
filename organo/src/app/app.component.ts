ca9e4d4d0cb7c99b764c74eaaff92a066dffe518
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-05-19 13:56:10 -0300

    feat: componente livro

--- organo/src/app/app.component.ts
+++ organo/src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
+import { LivroComponent } from './components/livro/livro.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent],
+  imports: [
+    RouterOutlet,
+    CabecalhoComponent,
+    RodapeComponent,
+    LivroComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
