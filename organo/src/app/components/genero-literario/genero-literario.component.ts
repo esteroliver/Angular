ab2f60f68fd1d777f0ca62b3e80683c188f86fe9
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-05-21 08:42:47 -0300

    style: estilizando a aplicação com ngClass

--- organo/src/app/components/genero-literario/genero-literario.component.ts
+++ organo/src/app/components/genero-literario/genero-literario.component.ts
@@ -1,11 +1,15 @@
 import {Component, InputSignal} from '@angular/core';
 import { LivroComponent } from '../livro/livro.component';
 import { input } from '@angular/core';
-import {ListaGeneroLiterario} from '../lista-livros/lista-genero';
+import { ListaGeneroLiterario } from '../lista-livros/lista-genero';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-genero-literario',
-  imports: [LivroComponent],
+  imports: [
+    LivroComponent,
+    CommonModule
+  ],
   templateUrl: './genero-literario.component.html',
   styleUrl: './genero-literario.component.css'
 })
