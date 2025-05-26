3faaec6d16428f14371b6c06693cbcf15a20147f
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-05-26 14:03:17 -0300

    style: estilo do banner

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BannerComponent } from './components/banner/banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    BannerComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
