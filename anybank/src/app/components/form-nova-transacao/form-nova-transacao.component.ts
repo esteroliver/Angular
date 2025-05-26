aae6f41cbde9e6054f572409c0051530eed44a10
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-05-26 16:24:56 -0300

    feat: utilizando ngModel

--- anybank/src/app/components/form-nova-transacao/form-nova-transacao.component.ts
+++ anybank/src/app/components/form-nova-transacao/form-nova-transacao.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './form-nova-transacao.component.html',
   styleUrl: './form-nova-transacao.component.css'
 })
 export class FormNovaTransacaoComponent {
+  valorTransacao = "";
+  tipoTransacao = "";
 
+  submeterTransacao(){
+    console.log(this.valorTransacao + " " + this.tipoTransacao);
+  }
 }
