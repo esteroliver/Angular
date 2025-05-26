230694d1db7af6c2ea2a67784f7df5777295050e
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-05-26 14:39:32 -0300

    style: componente para nova transação

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component';
+import { FormNovaTransacaoComponent } from './components/form-nova-transacao/form-nova-transacao.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    BannerComponent
+    BannerComponent,
+    FormNovaTransacaoComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
