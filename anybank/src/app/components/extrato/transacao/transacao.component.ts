57d8befa4a486c18e51b18f7a69cc48426ab42ca
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-06-02 12:30:53 -0300

    feat: finalizando as funcionalidades do componente extrato

--- anybank/src/app/components/extrato/transacao/transacao.component.ts
+++ anybank/src/app/components/extrato/transacao/transacao.component.ts
@@ -1,12 +1,14 @@
-import { CurrencyPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { TipoTransacao, Transacao } from '../../../modelos/transacao';
 
 @Component({
   selector: 'app-transacao',
-  imports: [ CurrencyPipe ],
+  imports: [ CurrencyPipe, DatePipe ],
   templateUrl: './transacao.component.html',
   styleUrl: './transacao.component.css'
 })
 export class TransacaoComponent {
-
+  transacao = input.required<Transacao>();
+  tipoTransacao = TipoTransacao;
 }
